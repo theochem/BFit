dd56fc5bc4b28044569a48bccf6a82857da60fb6
Farnaz Heidar Zadeh <farnazhz@gmail.com>
2015-05-22 12:06:49 -0400

    Fixed the bugs in the atomic_density.py and added new tests. The density integrates correctly now.

--- atomic_density.py
+++ atomic_density.py
@@ -11,15 +11,13 @@ import sympy as sp
 
 import fitting.io.slater_basic as sb
 
-#elementFile = "/Users/Alireza/Desktop/neutral/be"
-
 
 class Atomic_Density():
     """
     Insert Documentations
     """
-    def __init__(self, file, grid):
-        self.VALUES = sb.load_slater_basis(file)
+    def __init__(self, file_name, grid):
+        self.VALUES = sb.load_slater_basis(file_name)
         self.GRID = grid
         self.ALL_SLATOR_ORBITALS = self.slator_dict()
 
@@ -32,7 +30,17 @@ class Atomic_Density():
         :param r: distance form the nuclei
         :return: returns a number or an array depending on input values
         """
-        return ((2 * exponent)**quantumNum)   *    np.sqrt(((2 * exponent) / scipy.misc.factorial(2 * quantumNum)))    *      (r ** (quantumNum - 1)) * (np.exp(-exponent * r))
+        assert exponent.shape == quantumNum.shape
+        assert exponent.shape[1] == 1
+        assert quantumNum.shape[1] == 1
+        assert r.shape[1] == 1
+
+        normalization = ((2 * exponent)**quantumNum) * np.sqrt(((2 * exponent) / scipy.misc.factorial(2 * quantumNum)))
+        assert normalization.shape == exponent.shape
+        pre_factor = np.transpose(r ** (np.ravel(quantumNum) - 1))
+        slater =  pre_factor * (np.exp(-exponent * np.transpose(r)))
+        slater *= normalization
+        return slater
 
     def slator_dict(self):
         """
@@ -44,12 +52,13 @@ class Atomic_Density():
 
         :return: row = number of points, column = number of slater equations
         """
-        dict = {x[1]:0 for x in self.VALUES['orbitals'] }
-
-        for subshell in dict:
-            dict[subshell] = np.transpose(self.slator_type_orbital(self.VALUES['orbitals_exp'][subshell], self.VALUES['basis_numbers'][subshell], self.GRID ))
-
-        return dict
+        dict_orbital = {x[1]:0 for x in self.VALUES['orbitals'] }
+        for subshell in dict_orbital.keys():
+            exponents = self.VALUES['orbitals_exp'][subshell]
+            basis_numbers = self.VALUES['basis_numbers'][subshell]
+            slater = self.slator_type_orbital(exponents, basis_numbers, self.GRID)
+            dict_orbital[subshell] = np.transpose(slater)
+        return dict_orbital
 
     def all_coeff_matrix(self, subshell):
         """
@@ -89,7 +98,6 @@ class Atomic_Density():
         :return: array where row = number of points and column = number of phi/orbitals.
                 For example, beryllium will have row = # of points and column = 2 (1S and 2S)
         """
-
         return np.dot(self.ALL_SLATOR_ORBITALS[subshell], self.all_coeff_matrix(subshell))
 
     def phi_matrix(self): #connect all phis together
@@ -106,12 +114,12 @@ class Atomic_Density():
         counter = 0
         phi_matrix = 0
         for orbital in list_orbitals:
-            if orbital in self.VALUES['orbitals_exp']:
+            if orbital in self.VALUES['orbitals_exp'].keys():
                 if counter == 0:        #initilize array
                     phi_matrix = self.phi_LCAO(orbital)
                     counter += 1
                 else:
-                    array = np.concatenate((phi_matrix, self.phi_LCAO(orbital)), axis = 1)
+                    phi_matrix = np.concatenate((phi_matrix, self.phi_LCAO(orbital)), axis = 1)
         return phi_matrix
 
     def atomic_density(self):
@@ -123,5 +131,4 @@ class Atomic_Density():
         :return: the electron density where row = number of point
                  and column = 1
         """
-
         return np.dot(np.absolute(self.phi_matrix())**2, self.VALUES['orbitals_electron_array'] )
