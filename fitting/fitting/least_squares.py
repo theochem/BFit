5d838b2a650ac522f65f169e844ceef2e713c487
Ali-Tehrani <alirezatehrani@Hotmail.com>
2015-06-28 20:25:13 -0400

    - Blank

--- fitting/fitting/least_squares.py
+++ fitting/fitting/least_squares.py
@@ -5,15 +5,8 @@ from fitting.density import *
 import matplotlib.pyplot as plt
 import scipy.optimize
 
-def clenshaw_curtis(atomic_number): #remove zero
-    points = np.arange(0, 200)
-    points2 = np.arange(0, 300)
-    quadrature =  1/(2 * atomic_number) * (1 - np.cos( np.pi * points / (200 * 2)))
-    quadrature2 = 25 * (1 - np.cos(np.pi * points2 / 600))
-    quadrature = np.append(quadrature, quadrature2)
-    quadrature = np.append(quadrature, [50, 75, 100])
-    return np.sort(quadrature)
-
+#647 606 1422
+r"""
 Be_grid = clenshaw_curtis(4)
 Be_grid = Be_grid.reshape((len(Be_grid), 1))
 
@@ -26,19 +19,20 @@ be_rho = be.atomic_density()
 
 
 def func(x):
-    rho, grid, UGBS = be_rho, Be_grid, be_UGBS
+    rho, grid, UGBS_List = be_rho, Be_grid, be_UGBS
     inner = x * np.exp(-2 * UGBS * grid**2) # 503 x 25
     sum = np.sum(inner, axis=1)
     sum = np.reshape(sum, (503, 1))
     diff = rho - sum # 503 x 503
     return np.sum(diff**2)
 
-"""
+r"""
+r"""
 list = []
 for x in np.linspace(0, 5, num=100):
     list.append(func(x, args))
 plt.plot(list)
-plt.show()"""
+plt.show()
 
 x0 = np.linspace(0, 1, num = 25)
 c = scipy.optimize.fmin_l_bfgs_b(func, x0,  bounds=[(0, None) for x in range(0, 25)], approx_grad=True )
@@ -71,3 +65,48 @@ print(np.trapz(grid**2 * density_approx, grid) )       #number of electrons by i
 be = Atomic_Density(file_path, grid.reshape(len(grid), 1))
 density = np.ravel(be.atomic_density())
 print(np.trapz(np.power(grid, 2) * density, grid)  )                  #number of electrons by integrating the true density
+r"""
+
+def clenshaw_curtis(atomic_number, gridpoints1, gridpoints2):
+    """
+    This does not include two zeroes
+    :param atomic_number:
+    :param gridpoints1:
+    :param gridpoints2:
+    :return:
+    """
+    points = np.arange(0, gridpoints1)
+    points2 = np.arange(1, gridpoints2)
+
+    quadrature =  1/ (2 * atomic_number) * (1 - np.cos( np.pi * points / (gridpoints1 * 2)))
+    quadrature2 = 25 * (1 - np.cos(np.pi * points2 / (gridpoints2 * 2)))
+
+    quadrature = np.concatenate((quadrature, quadrature2, [50, 75, 100]), axis=1)
+    return np.sort(quadrature)
+
+
+print(np.shape(clenshaw_curtis(4, 200, 300)))
+
+Be_grid = clenshaw_curtis(4, 200, 300)
+Be_grid = Be_grid.reshape((len(Be_grid), 1))
+
+a = gbasis.UGBSBasis("be")
+be_UGBS = (a.exponents("s"))
+file_path = r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater"
+be = atomic_slater_density.Atomic_Density(r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater", Be_grid)
+be_rho = be.atomic_density()
+
+def total_density_cost_function(x, *args):
+    """
+
+    :param r:
+    :param args:
+    :return:
+    """
+
+    UGBS, grid, rho = args[0], args[1], args[2]
+    gaussian = x * np.exp(-2 * UGBS * grid**2) # 503 x 25
+    sum_gaussians = np.sum(gaussian, axis=1)
+
+
+print(total_density_cost_function(1, (be_UGBS, Be_grid, be_rho)))
