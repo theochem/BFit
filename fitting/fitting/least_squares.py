6a47d5cea70fa8aa83033eb0ade4b78e4d5416ae
Ali-Tehrani <alirezatehrani@Hotmail.com>
2015-07-13 01:28:44 -0400

    - Added NNLS

--- fitting/fitting/least_squares.py
+++ fitting/fitting/least_squares.py
@@ -5,75 +5,10 @@ from fitting.density import *
 import matplotlib.pyplot as plt
 import scipy.optimize
 
-#647 606 1422
-r"""
-Be_grid = clenshaw_curtis(4)
-Be_grid = Be_grid.reshape((len(Be_grid), 1))
-
-
-a = gbasis.UGBSBasis("be")
-be_UGBS = (a.exponents("s"))
-file_path = r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater"
-be = atomic_slater_density.Atomic_Density(r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater", Be_grid)
-be_rho = be.atomic_density()
-
-
-def func(x):
-    rho, grid, UGBS_List = be_rho, Be_grid, be_UGBS
-    inner = x * np.exp(-2 * UGBS * grid**2) # 503 x 25
-    sum = np.sum(inner, axis=1)
-    sum = np.reshape(sum, (503, 1))
-    diff = rho - sum # 503 x 503
-    return np.sum(diff**2)
-
-r"""
-r"""
-list = []
-for x in np.linspace(0, 5, num=100):
-    list.append(func(x, args))
-plt.plot(list)
-plt.show()
-
-x0 = np.linspace(0, 1, num = 25)
-c = scipy.optimize.fmin_l_bfgs_b(func, x0,  bounds=[(0, None) for x in range(0, 25)], approx_grad=True )
-print(c)
-coeff = c[0]
-
-
-grid = np.arange(0.0, 10.0, 0.0001)
-print(np.shape(coeff), np.shape(be_UGBS), np.shape(grid))
-
-def f(opt_coeffs, grid):
-    grid = np.reshape(grid, (len(grid), 1))  #100000 x 1
-    print(np.shape(coeff * np.exp(be_UGBS * -2 * grid**2)))    #(100000 x 25)
-    return np.sum(coeff * np.exp(be_UGBS * -2 * grid**2), axis = 1) #(100000 x 1)
-print(coeff)
-coeff = np.array([  5.46216373e+00,   0.00000000e+00,   0.00000000e+00,
-         3.88524190e+00,   4.84911435e+00,   5.04914030e+00,
-         7.77072403e+00,   1.14552666e+01,   1.54129327e+01,
-         2.13043839e+01,   2.96460347e+01,   3.97163402e+01,
-         5.22680484e+01,   6.46277459e+01,   6.95888197e+01,
-         6.10717675e+01,   3.96714102e+01,   1.16335009e+01,
-         1.39023514e-01,   9.61228602e-02,   4.43567089e-04,
-         2.14737164e-01,   1.96237121e-01,   1.63525742e-02,
-         0.00000000e+00])
-density_approx = f(coeff, grid)   #opt_coeffs refers to the optimized coefficients
-# turn the density approximate into 1D array, if yours is 2D array (i.e. column array)
-density_approx = np.ravel(density_approx)
-print(np.trapz(grid**2 * density_approx, grid) )       #number of electrons by integrating the approximate model
-
-be = Atomic_Density(file_path, grid.reshape(len(grid), 1))
-density = np.ravel(be.atomic_density())
-print(np.trapz(np.power(grid, 2) * density, grid)  )                  #number of electrons by integrating the true density
-r"""
 
 def clenshaw_curtis(atomic_number, gridpoints1, gridpoints2):
     """
-    This does not include two zeroes
-    :param atomic_number:
-    :param gridpoints1:
-    :param gridpoints2:
-    :return:
+    does not include zero
     """
     points = np.arange(0, gridpoints1)
     points2 = np.arange(1, gridpoints2)
@@ -84,29 +19,84 @@ def clenshaw_curtis(atomic_number, gridpoints1, gridpoints2):
     quadrature = np.concatenate((quadrature, quadrature2, [50, 75, 100]), axis=1)
     return np.sort(quadrature)
 
-
-print(np.shape(clenshaw_curtis(4, 200, 300)))
-
+# Create A Clenshaw Grid Based on Beryllium that has 502 points,
 Be_grid = clenshaw_curtis(4, 200, 300)
-Be_grid = Be_grid.reshape((len(Be_grid), 1))
+Be_grid = Be_grid.reshape((len(Be_grid), 1)) # 502, 1
 
+#Obtain UGBS for Beryllium
 a = gbasis.UGBSBasis("be")
 be_UGBS = (a.exponents("s"))
+
+#Obtain Electron Density for be from the clenshaw grid
 file_path = r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater"
 be = atomic_slater_density.Atomic_Density(r"\Users\Alireza\PycharmProjects\fitting\fitting\data\examples\be.slater", Be_grid)
 be_rho = be.atomic_density()
 
+#Optional
+#Plot the Electron Density for beryllium from the clenshaw grid
+plt.plot((be_rho))
+plt.show()
+
+
+#############################################################################################################################
+########################################## FMIN_L_BFGS_B ###################################################################
+#############################################################################################################################
 def total_density_cost_function(x, *args):
     """
-
-    :param r:
-    :param args:
-    :return:
     """
-
     UGBS, grid, rho = args[0], args[1], args[2]
     gaussian = x * np.exp(-2 * UGBS * grid**2) # 503 x 25
     sum_gaussians = np.sum(gaussian, axis=1)
+    sum_gaussians = np.reshape(sum_gaussians, (len(sum_gaussians), 1))
+    residual = rho - sum_gaussians
+    sum_squared_residuals = np.sum(residual**2)
+    return sum_squared_residuals
+
+print(total_density_cost_function(0, be_UGBS, Be_grid, be_rho) == np.sum(be_rho**2 ))
+# Initial Guess
+x0 = np.linspace(0, 1, num = 25)
+#This Is The Optimization that is Used
+c = scipy.optimize.fmin_l_bfgs_b(total_density_cost_function, x0, args=(be_UGBS, Be_grid, be_rho),  bounds=[(0, None) for x in range(0, 25)], approx_grad=True )
+coeff = c[0]
+
+# Another Grid that takes small increments towards 10
+grid = np.arange(0.0, 10.0, 0.0001)
+def f(opt_coeffs, grid):
+    grid = np.reshape(grid, (len(grid), 1))  #100000 x 1
+    print(np.shape(coeff * np.exp(be_UGBS * -2 * grid**2)))    #(100000 x 25)
+    return np.sum(coeff * np.exp(be_UGBS * -2 * grid**2), axis = 1)
+
+density_approx = f(coeff, Be_grid)   #opt_coeffs refers to the optimized coefficients
+# turn the density approximate into 1D array, if yours is 2D array (i.e. column array)
+density_approx = np.ravel(density_approx)
+#print("density approx", np.trapz(Be_grid**2 * density_approx, Be_grid) )       #number of electrons by integrating the approximate model
 
 
-print(total_density_cost_function(1, (be_UGBS, Be_grid, be_rho)))
+#############################################################################################################################
+########################################## NON-NEGATIVE LEAST SQUARES #######################################################
+#############################################################################################################################
+"""
+Performing non-negative least squares solver
+where b is the electron density and A is the """
+def total_density_cost_function(x, *args):
+
+    UGBS, grid, rho = args[0], args[1], args[2]
+    gaussian = np.exp(-2 * UGBS * grid**2) # 503 x 25
+    return gaussian
+
+# This uses clenshaw grid and electron density
+total = total_density_cost_function(1, be_UGBS, Be_grid, be_rho)
+print(np.shape(total), np.shape(be_rho))
+solution = scipy.optimize.nnls(total, be_rho)[0]
+print(solution, coeff)
+density_approx2 = f(solution, Be_grid)
+plt.plot((density_approx), "b")
+plt.plot(density_approx2, "r")
+plt.plot(be_rho, "g")
+plt.show()
+density_approx = np.reshape(density_approx, (len(density_approx), 1))
+print(np.shape(be_rho), np.shape(density_approx))
+print(np.absolute(np.sum(be_rho - density_approx)))
+density_approx2 = np.reshape(density_approx2, (len(density_approx2), 1))
+print(np.sum(be_rho - density_approx2))
+print("Density approx", np.shape(density_approx2))
