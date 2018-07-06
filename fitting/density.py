Fix core/valence density computation & add test

fitting/density.py
@@ -145,68 +145,24 @@ class AtomicDensity(object):
             phi_matrix[:, index] = np.dot(slater, self.orbitals_coeff[orbital]).ravel()
         return phi_matrix
 
-    def atomic_density(self, points):
+    def atomic_density(self, points, mode="total"):
+        """Compute atomic density on the given 1D grid points array.
+
+        Parameters
+        ----------
+        points
         """
-        By Taking the occupation numbers and multiplying it
-        by the corresponding absolute, squared phi to obtain
-        electron least_squares(rho).
+        if mode not in ["total", "valence", "core"]:
+            raise ValueError("Argument mode not recognized!")
 
-        :return: the electron least_squares where row = number of point
-                 and column = 1
-        """
-        dot = np.dot(self.phi_matrix(points)**2, self.orbitals_occupation)
-        return np.ravel(dot) / (4. * np.pi)
-
-    def atomic_density_core_valence(self):
-        """
-        Calculates Atomic Density for core and valence electrons.
-
-        """
-        def energy_homo():
-            """
-            A helper function that finds the HOMO energy of the element
-
-            Returns
-            --------
-            float
-                Energy Of Homo
-
-            """
-            # Initilize the energy from first value from the list.
-            energy_homo = self.orbitals_energy['S'][0]
-            for orbital, list_energy in self.orbitals_energy.items():
-                max_of_list = np.max(list_energy)
-                if max_of_list > energy_homo:
-                    energy_homo = max_of_list
-            return energy_homo
-
-        energy_homo = energy_homo()
-
-        def join_energy():
-            """
-            A helper function to join all of the energy levels into one array
-
-            Returns
-            -------
-            list
-                All energy levels in one list.
-
-            """
-            joined_array = np.array([])
-            for orbital in ['S', 'P', 'D', 'F']:
-                if orbital in self.orbitals_energy:
-                    orbital_energy = self.orbitals_energy[orbital]
-                    joined_array = np.hstack([joined_array, orbital_energy])
-            return joined_array
-
-        phi_matrix = self.phi_matrix()
-        energy_difference = join_energy() - energy_homo
-
-        absolute_squared = 1 - np.exp((-1) * np.absolute(energy_difference**2))
-        core = absolute_squared * np.absolute(phi_matrix)**2
-
-        absolute_squared_val = np.exp((-1) * np.absolute(energy_difference**2))
-        valence = absolute_squared_val * np.absolute(phi_matrix)**2
-
-        return(np.dot(core, self.orbitals_electron_array),
-               np.dot(valence, self.orbitals_electron_array))
+        # compute orbital occupation numbers
+        orb_occs = self.orbitals_occupation
+        if mode == "valence":
+            orb_homo = self.orbitals_energy[len(self.orbitals_occupation) - 1]
+            orb_occs = orb_occs * np.exp(-(self.orbitals_energy - orb_homo)**2)
+        elif mode == "core":
+            orb_homo = self.orbitals_energy[len(self.orbitals_occupation) - 1]
+            orb_occs = orb_occs * (1. - np.exp(-(self.orbitals_energy - orb_homo)**2))
+        # compute density
+        dens = np.dot(self.phi_matrix(points)**2, orb_occs).ravel() / (4 * np.pi)
+        return dens

fitting/utils/io.py
@@ -131,8 +131,8 @@ def load_slater_wfn(file_name):
 
         orbitals = []
         orbitals_basis = {'S': [], 'P': [], 'D': [], "F": []}
-        orbitals_cusp = {'S': 0, 'P': 0, 'D': 0, "F": 0}
-        orbitals_energy = {'S': [], 'P': [], 'D': [], "F": []}
+        orbitals_cusp = []
+        orbitals_energy = []
         orbitals_exp = {'S': [], 'P': [], 'D': [], "F": []}
         orbitals_coeff = {}
 
@@ -149,9 +149,9 @@ def load_slater_wfn(file_name):
 
                 # Get Energy, Cusp Levels
                 line = f.readline()
-                orbitals_energy[subshell] = [float(x) for x in line.split()[1:]]
+                orbitals_energy.extend([float(x) for x in line.split()[1:]])
                 line = f.readline()
-                orbitals_cusp[subshell] = [float(x) for x in line.split()[1:]]
+                orbitals_cusp.extend([float(x) for x in line.split()[1:]])
                 line = f.readline()
 
                 # Get Exponents, Coefficients, Orbital Basis
@@ -170,9 +170,8 @@ def load_slater_wfn(file_name):
     data = {'configuration': configuration,
             'energy': energy,
             'orbitals': orbitals,
-            'orbitals_energy':
-            {key: np.asarray(value) for key, value in orbitals_energy.items() if value != []},
-            'orbitals_cusp': orbitals_cusp,
+            'orbitals_energy': np.array(orbitals_energy)[:, None],
+            'orbitals_cusp': np.array(orbitals_cusp)[:, None],
             'orbitals_basis': orbitals_basis,
             'orbitals_exp':
             {key: np.asarray(value).reshape(len(value), 1) for key, value in orbitals_exp.items()

fitting/utils/test/test_io.py
@@ -42,8 +42,8 @@ def test_parsing_slater_density_be():
 
     # Check basis of S orbitals
     assert be['orbitals'] == ['1S', '2S']
-    assert np.all(be['orbitals_cusp']['S'] == [1.0001235, 0.9998774])
-    assert np.all(be['orbitals_energy']['S'] == [-4.7326699, -0.3092695])
+    assert np.all(abs(be['orbitals_cusp'] - np.array([1.0001235, 0.9998774])[:, None]) < 1.e-6)
+    assert np.all(abs(be['orbitals_energy'] - np.array([-4.7326699, -0.3092695])[:, None]) < 1.e-6)
     assert be['orbitals_basis']['S'] == ['1S', '1S', '1S', '1S', '1S', '1S', '2S', '1S']
     assert len(be['orbitals_occupation']) == 2
     assert (be['orbitals_occupation'] == np.array([[2], [2]])).all()
@@ -81,9 +81,12 @@ def test_parsing_slater_density_ag():
     # Check basis
     assert ag['orbitals_basis']['P'] == ['2P', '3P', '3P', '2P', '3P', '3P', '3P',
                                          '3P', '2P', '2P', '2P']
-    assert ag['orbitals_cusp']['P'] == [1.0008130, 1.0008629, 0.9998751]
-    assert np.all(ag['orbitals_cusp']['D'] == [0.9991182, 1.0009214])
-    assert np.all(ag['orbitals_energy']['P'] == [-125.1815809, -21.9454343, -2.6768201])
+    cusp = np.array([1.0002457, 1.0000318, 1.0004188, 1.0004755, 1.0007044,
+                     1.0008130, 1.0008629, 0.9998751, 0.9991182, 1.0009214])[:, None]
+    energy = np.array([-913.8355964, -134.8784068, -25.9178242, -4.0014988, -0.2199797,
+                       -125.1815809, -21.9454343, -2.6768201, -14.6782003, -0.5374007])[:, None]
+    assert (abs(ag['orbitals_cusp'] - cusp) < 1.e-6).all()
+    assert (abs(ag['orbitals_energy'] - energy) < 1.e-6).all()
 
     # Check exponents of D orbitals
     exp_D = np.array([53.296212, 40.214567, 21.872645, 17.024065, 10.708021, 7.859216, 5.770205,
@@ -114,8 +117,8 @@ def test_parsing_slater_density_ne():
     assert ne['energy'] == [128.547098140]
 
     # Check orbiral energy and cusp
-    assert ne['orbitals_energy']['P'] == [-0.8504095]
-    assert ne['orbitals_cusp']['P'] == [1.0000509]
+    assert (abs(ne['orbitals_energy'] - np.array([-32.7724425, -1.9303907, -0.8504095])[:, None]) < 1.e-6).all()
+    assert (abs(ne['orbitals_cusp'] - np.array([1.0000603, 0.9996584, 1.0000509])[:, None]) < 1.e-6).all()
 
     # Check basis
     assert ne['orbitals_basis']['P'] == ['3P', '2P', '3P', '2P', '2P', '2P', '2P']