4c865f249e93b71db9cf297a58d0c769a7b792bc
Ali-Tehrani <alirezatehrani24@gmail.com>
2020-01-27 10:32:30 -0500

    Finish documentation for grid.py.

--- fitting/grid.py
+++ fitting/grid.py
@@ -19,7 +19,29 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # ---
-"""Grid Module."""
+"""
+Grid Module - Model the grid and integration methods of the model and true probability distributions.
+
+Methods
+-------
+There are three classes:
+
+ClenshawRadialGrid -
+    Models a One-dimensional grid via Clenshaw-Curtis pattern.
+    Integration is done via 'np.trapz' method (spherical coordinates, optional).
+    Intended for Atomic fitting.
+
+UniformRadialGrid -
+    Uniform (equal spacing), one-dimensional grid.
+    Integration is done via 'np.trapz' method (spherical coordinates, optional).
+    Intended for Atomic fitting.
+
+CubicGrid -
+    Uniform (equal spacing), three-dimensional grid.
+    Integration is done via Riemannian sums.
+    Intended for Molecular fitting.
+
+"""
 
 
 import numpy as np
@@ -28,17 +50,35 @@ import numpy as np
 __all__ = ["ClenshawRadialGrid", "UniformRadialGrid", "CubicGrid"]
 
 
-class BaseRadialGrid(object):
-    r"""Base Radial Grid Class."""
+class _BaseRadialGrid(object):
+    r"""
+    Radial Grid Base Class.
+
+    Attributes
+    ----------
+    points : ndarray, (N,)
+        The radial grid points of a one-dimensional grid with N points.
+    spherical : bool
+        If true, then trapezoidal integration is done spherically (ie with a factor of :math:`4 \pi r^2`).
+
+    Methods
+    -------
+    integrate(arr)
+        Integrate array `arr` defined over "point" array using trapezoidal integration.
+
+    """
 
     def __init__(self, points, spherical=True):
         """
+        Construct BaseRadialGrid object.
+
         Parameters
         ----------
         points : ndarray, (N,)
             The radial grid points.
         spherical : bool, optional
             If `True`, the spherical integration of function is computed.
+
         """
         if not isinstance(points, np.ndarray) or points.ndim != 1:
             raise TypeError("Argument points should be a 1D numpy array.")
@@ -56,7 +96,7 @@ class BaseRadialGrid(object):
         return self._spherical
 
     def __len__(self):
-        """Number of grid points."""
+        """Return number of grid points."""
         return self._points.shape[0]
 
     def integrate(self, arr):
@@ -76,6 +116,7 @@ class BaseRadialGrid(object):
         -------
         value : float
             The value of integral.
+
         """
         if arr.shape != self.points.shape:
             raise ValueError("The argument arr should have {0} shape!".format(self.points.shape))
@@ -86,16 +127,29 @@ class BaseRadialGrid(object):
         return value
 
 
-class UniformRadialGrid(BaseRadialGrid):
+class UniformRadialGrid(_BaseRadialGrid):
     r"""Uniformly Distributed Radial Grid Class.
 
-    The radial grid points are equally-spaced in `[0, max_points]` interval, i.e.,
+    The grid points are equally-spaced in :math:`[0, max_points)` interval.
 
-    .. math::
+    Attributes
+    ----------
+    points : ndarray, (N,)
+        The radial grid points of a one-dimensional grid with N points.
+    spherical : bool
+        If true, then trapezoidal integration is done spherically (ie with a factor of :math:`4 \pi r^2`).
+
+    Methods
+    -------
+    integrate(arr)
+        Integrate array `arr` defined over "point" array using trapezoidal integration.
+
     """
 
     def __init__(self, num_pts, min_radii=0., max_radii=100., spherical=True):
         """
+        Construct the UniformRadialGrid object.
+
         Parameters
         ----------
         num_pts : int
@@ -106,6 +160,7 @@ class UniformRadialGrid(BaseRadialGrid):
             The largest radial grid point.
         spherical : bool, optional
             If `True`, the spherical integration of function is computed.
+
         """
         if not isinstance(num_pts, int) or num_pts <= 0:
             raise TypeError("Argument num_pts should be a number.")
@@ -121,15 +176,41 @@ class UniformRadialGrid(BaseRadialGrid):
         super(UniformRadialGrid, self).__init__(points, spherical)
 
 
-class ClenshawRadialGrid(BaseRadialGrid):
+class ClenshawRadialGrid(_BaseRadialGrid):
     r"""Clenshaw-Curtis Radial Grid Class.
 
-    The Clenshaw-Curtis grid places more points close to the origin within [0, inf) interval,
-    and add sparse points further away.
+    The Clenshaw-Curtis grid places more points closer to the origin of the interval :math:`[0, \inf).`
+    It is defined as follows. Let :math:`Z, m, n` be the atomic number, number of points near origin,
+    and the number of points far from the origin, respectively.
+
+    Then each point :math:`r_p` of the Clenshaw radial grid is defined as:
+
+    .. math::
+        \begin{eqnarray}
+            r_p = \frac{1}{2Z} \bigg(1 - \cos\bigg(\frac{\pi p}{400} \bigg)\bigg)  & p = 0, 1, \cdots, m - 1 \\
+            r_p = 25 \bigg(1 - \cos\bigg(\frac{\pi p}{600} \bigg)\bigg) & p = 0, 1, \cdocts, n - 1\\
+        \end{eqnarray}
+
+    Attributes
+    ----------
+    points : ndarray, (N,)
+        The radial grid points of a one-dimensional grid with N points.
+    atomic_number :
+        Return the atomic number.
+    spherical : bool
+        If true, then trapezoidal integration is done spherically (ie with a factor of :math:`4 \pi r^2`).
+
+    Methods
+    -------
+    integrate(arr)
+        Integrate array `arr` defined over "point" array using trapezoidal integration.
+
     """
 
     def __init__(self, atomic_number, num_core_pts, num_diffuse_pts, extra_pts=None, spherical=True):
         r"""
+        Construct ClenshawRadialGrid grid object.
+
         Parameters
         ----------
         atomic_number : int
@@ -142,6 +223,7 @@ class ClenshawRadialGrid(BaseRadialGrid):
             Additional points to be added to the grid, commonly points far away from origin.
         spherical : bool, optional
             If `True`, the spherical integration of function is computed.
+
         """
         if not isinstance(atomic_number, int) or atomic_number <= 0:
             raise TypeError("Argument atomic_number should be a positive integer.")
@@ -169,7 +251,7 @@ class ClenshawRadialGrid(BaseRadialGrid):
 
     @property
     def atomic_number(self):
-        """The atomic number."""
+        """Return the atomic number."""
         return self._atomic_number
 
     def _get_points(self, num_pts, mode="core"):
@@ -197,6 +279,7 @@ class ClenshawRadialGrid(BaseRadialGrid):
         -------
         points : ndarray, (N,)
             The 1D array of grid points.
+
         """
         if mode.lower() == "core":
             points = 1. - np.cos(0.5 * np.pi * np.arange(0, num_pts) / num_pts)
@@ -209,10 +292,27 @@ class ClenshawRadialGrid(BaseRadialGrid):
 
 
 class CubicGrid(object):
-    r"""Equally-Spaced 3D Cubic Grid Class."""
+    r"""
+    Equally-Spaced 3D Cubic Grid Class.
+
+    Attributes
+    ----------
+    points : ndarray, (N, 3)
+        The three-dimensional array containing the `N` grid points that are uniform.
+    step : float
+        The positive number representing the step-size of any two consequent grid points.
+
+    Methods
+    -------
+    integrate(arr) :
+        Integrate an array `arr` defined over the `points` using Riemannian sum.
+
+    """
 
     def __init__(self, smallest_pnt, largest_pnt, step_size):
         """
+        Construct the CubicGrid object.
+
         Parameters
         ----------
         smallest_pnt : float
@@ -221,6 +321,7 @@ class CubicGrid(object):
             The largest point on any axis in the 3D cubic grid.
         step_size : float
             The step-size between two consecutive points on any axis in the 3D cubic grid.
+
         """
         if not isinstance(smallest_pnt, (int, float)):
             raise TypeError("Argument smallest_pt should be a positive number.")
@@ -244,16 +345,16 @@ class CubicGrid(object):
 
     @property
     def points(self):
-        """Cubic grid points."""
+        """Return cubic grid points."""
         return self._points
 
     @property
     def step(self):
-        """The step size between to consecutive points."""
+        """Return the step size between to consecutive points."""
         return self._step
 
     def __len__(self):
-        """Number of grid points."""
+        """Return the number of grid points."""
         return self._points.shape[0]
 
     def integrate(self, arr):
@@ -272,6 +373,7 @@ class CubicGrid(object):
         -------
         value : float
             The value of integral.
+
         """
         if arr.shape != (len(self),):
             raise ValueError("Argument arr should have ({0},) shape.".format(len(self)))
