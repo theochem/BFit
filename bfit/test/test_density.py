f271c7e3ee00b1897e5104985f6bb2808cce3306
Ali-Tehrani <alirezatehrani24@gmail.com>
2020-05-23 12:23:07 -0400

    Add tests for kinetic energy

--- bfit/test/test_density.py
+++ bfit/test/test_density.py
@@ -29,10 +29,13 @@ from numpy.testing import assert_equal, assert_almost_equal, assert_raises
 from bfit.density import AtomicDensity
 
 
-def slater(e, n, r):
+def slater(e, n, r, derivative=False):
     """Calculates single normalized slater function at a given point."""
     norm = np.power(2. * e, n) * np.sqrt(2. * e / np.math.factorial(2. * n))
     slater = norm * np.power(r, n - 1) * np.exp(-e * r)
+
+    if derivative:
+        return (((n - 1) / r) - e) * slater
     return slater
 
 
@@ -51,6 +54,92 @@ def test_slater_type_orbital_be():
     expected = np.array([[slater(exps[0, 0], nums[0, 0], 1.), slater(exps[1, 0], nums[1, 0], 1.)],
                          [slater(exps[0, 0], nums[0, 0], 2.), slater(exps[1, 0], nums[1, 0], 2.)]])
     assert_almost_equal(orbitals, expected, decimal=6)
+
+
+def test_derivative_slater_type_orbital_be():
+    # load Be atomic wave function
+    be = AtomicDensity("Be")
+    # check values of a single orbital at r=1.0
+    orbital = be.derivative_slater_type_orbital(np.array([[12.683501]]),
+                                                np.array([[1]]), np.array([1]))
+    assert_almost_equal(orbital, slater(12.683501, 1, 1.0, derivative=True), decimal=6)
+    # check values of a single orbital at r=2.0
+    orbital = be.derivative_slater_type_orbital(np.array([[0.821620]]), np.array([[2]]),
+                                                np.array([2]))
+    assert_almost_equal(orbital, slater(0.821620, 2, 2.0, derivative=True), decimal=6)
+    # check value of tow orbitals at r=1.0 & r=2.0
+    exps, nums = np.array([[12.683501], [0.821620]]), np.array([[1], [2]])
+    orbitals = be.derivative_slater_type_orbital(exps, nums, np.array([1., 2.]))
+    expected = np.array([[slater(exps[0, 0], nums[0, 0], 1., True),
+                          slater(exps[1, 0], nums[1, 0], 1., True)],
+                         [slater(exps[0, 0], nums[0, 0], 2., True),
+                          slater(exps[1, 0], nums[1, 0], 2., True)]])
+    assert_almost_equal(orbitals, expected, decimal=6)
+
+
+def test_positive_definite_kinetic_energy_he():
+    # load he atomic wave function
+    he = AtomicDensity("he")
+    # compute density on an equally distant grid
+    grid = np.arange(0., 30.0, 0.0001)
+    energ = he.lagrangian_kinetic_energy(grid)
+    integral = np.trapz(energ, grid)
+    assert np.all(np.abs(integral - he.energy[0]) < 1e-4)
+
+
+def test_positive_definite_kinetic_energy_be():
+    # load be atomic wave function
+    be = AtomicDensity("be")
+    # compute density on an equally distant grid
+    grid = np.arange(0., 30.0, 0.0001)
+    energ = be.lagrangian_kinetic_energy(grid)
+    integral = np.trapz(energ, grid)
+    assert np.all(np.abs(integral - be.energy[0]) < 1e-5)
+
+
+def test_positive_definite_kinetic_energy_most_atoms():
+    # load c atomic wave function
+    for atom in ["b", "cl", "ag"]:
+        adens = AtomicDensity(atom)
+        # compute density on an equally distant grid
+        grid = np.arange(0., 30., 0.0005)
+        energ = adens.lagrangian_kinetic_energy(grid)
+        integral = np.trapz(energ, grid)
+        assert np.all(np.abs(integral - adens.energy[0]) < 1e-3)
+
+
+def test_phi_derivative_lcao_b():
+    # load Be atomic wave function
+    b = AtomicDensity("b")
+    # check the values of the phi_matrix at point 1.0
+    phi_matrix = b.phi_matrix(np.array([1]), deriv=True)
+    def _slater_deriv(r):
+        # compute expected value of 1S
+        phi1S = slater(16.109305, 2, r, True) * -0.0005529 + slater(7.628062, 1, r, True) * -0.23501
+        phi1S += slater(6.135799, 2, r, True) * -0.1508924 + slater(4.167618, 1, r, True) * -0.64211
+        phi1S += slater(2.488602, 1, r, True) * -0.0011507 + slater(1.642523, 2, r, True) * -0.00086
+        phi1S += slater(0.991698, 1, r, True) * 0.0004712 + slater(0.787218, 1, r, True) * -0.000232
+        # compute expected value of 2S
+        phi2S = slater(16.109305, 2, r, True) * -0.0001239 + slater(7.628062, 1, r, True) * 0.012224
+        phi2S += slater(6.135799, 2, r, True) * 0.0355967 + slater(4.167618, 1, r, True) * -0.198721
+        phi2S += slater(2.488602, 1, r, True) * -0.5378967 + slater(1.642523, 2, r, True) * -0.11997
+        phi2S += slater(0.991698, 1, r, True) * 1.4382402 + slater(0.787218, 1, r, True) * 0.0299258
+        # compute expected value of 1P
+        phi3S = slater(12.135370, 3, r, True) * 0.0000599 + slater(5.508493, 2, r, True) * 0.0113751
+        phi3S += slater(3.930298, 3, r, True) * 0.0095096 + slater(2.034395, 2, r, True) * 0.1647518
+        phi3S += slater(1.301082, 2, r, True) * 0.3367860 + slater(0.919434, 2, r, True) * 0.4099162
+        phi3S += slater(0.787218, 2, r, True) * 0.1329396
+        return [phi1S, phi2S, phi3S]
+    # check shape & orbital values
+    assert_equal(phi_matrix.shape, (1, 3))
+    assert_almost_equal(phi_matrix, np.array([_slater_deriv(1)]), decimal=4)
+    # check the values of the phi_matrix at point 1.0, 2.0, 3.0
+    phi_matrix = b.phi_matrix(np.array([1., 2., 3.]), True)
+    # check shape & orbital values
+    assert_equal(phi_matrix.shape, (3, 3))
+    assert_almost_equal(phi_matrix[0, :], _slater_deriv(1), decimal=4)
+    assert_almost_equal(phi_matrix[1, :], _slater_deriv(2.), decimal=4)
+    assert_almost_equal(phi_matrix[2, :], _slater_deriv(3.), decimal=4)
 
 
 def test_coeff_matrix_be():
@@ -205,9 +294,20 @@ def test_atomic_density_c():
     assert_almost_equal(4 * np.pi * np.trapz(grid**2 * dens, grid), 6.0, decimal=6)
 
 
+def test_atomic_density_h():
+    r"""Test integration of atomic density."""
+    h = AtomicDensity("h")
+    grid = np.arange(0.0, 15.0, 0.0001)
+    dens = h.atomic_density(grid, mode="total")
+    print(np.trapz(np.exp(- 0.5641896 * grid) * grid**2. * 4. * np.pi, grid) * (2. * 0.5641896) * np.sqrt(2. * 0.5641896 / 2))
+    print("Asdsa")
+    assert_almost_equal((4 * np.pi) * np.trapz(dens * grid**2., grid), 1.0, decimal=6)
+
+
 def test_raises():
     assert_raises(TypeError, AtomicDensity, 25)
     assert_raises(TypeError, AtomicDensity, "be2")
-    assert_raises(ValueError, AtomicDensity.slater_orbital, np.array([[1]]), np.array([[2]]), np.array([[1.]]))
+    assert_raises(ValueError, AtomicDensity.slater_orbital, np.array([[1]]), np.array([[2]]),
+                  np.array([[1.]]))
     c = AtomicDensity("c")
     assert_raises(ValueError, c.atomic_density, np.array([[1.]]), "not total")
