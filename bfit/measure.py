17e25bf5080a1b16bfc09fde4d4ed11fb63d87e9
Ali-Tehrani <alirezatehrani24@gmail.com>
2021-11-22 12:39:11 -0500

    Add Tsallis measure to measure.py

--- bfit/measure.py
+++ bfit/measure.py
@@ -26,7 +26,8 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 
-__all__ = ["KLDivergence", "SquaredDifference"]
+
+__all__ = ["SquaredDifference", "KLDivergence", "TsallisDivergence"]
 
 
 class Measure(ABC):
@@ -213,3 +214,103 @@ class KLDivergence(Measure):
         if deriv:
             return value, -ratio
         return value
+
+
+class TsallisDivergence(Measure):
+    r"""Tsallis Divergence Class"""
+    def __init__(self, alpha=1.001, mask_value=1.e-12):
+        r"""
+        Construct the Kullback-Leibler class.
+
+        Parameters
+        ----------
+        alpha : float
+            The alpha parameter of the Tsallis divergence. If it tends towards
+            one, then Tsallis divergence approaches Kullback-Leibler.
+        mask_value : float, optional
+            The elements less than or equal to this number are masked in a division,
+            and then replaced with the value of one so that logarithm of one is zero.
+        """
+        self._alpha = alpha
+        self._mask_value = mask_value
+        if self._alpha < 0:
+            raise ValueError(f"Alpha parameter {alpha} should be positive.")
+        if np.abs(self._alpha - 1.0) < 1e-10:
+            raise ValueError(f"Alpha parameter {alpha} shouldn't be equal to one."
+                             f"Use Kullback-Leibler divergence instead.")
+        super(TsallisDivergence, self).__init__()
+
+    @property
+    def alpha(self):
+        r"""Alpha parameter of the Tsallis divergence."""
+        return self._alpha
+
+    @property
+    def mask_value(self):
+        r"""Masking value used when evaluating the measure."""
+        return self._mask_value
+
+    def evaluate(self, density, model, deriv=False):
+        r"""
+        The integrand of Tsallis divergence:
+
+        .. math::
+            \int_G \frac{1}{\alpha - 1} f(x) \bigg(\frac{f(x)}{g(x)}^{q- 1} - 1\bigg) dx,
+        where
+            :math:`f` is the true probability distribution,
+            :math:`g` is the model probability distribution.
+            :math:`G` is the grid being integrated on.
+
+        Parameters
+        ----------
+        density : ndarray(N,)
+            The exact density evaluated on the grid points.
+        model : ndarray(N,)
+            The model density evaluated on the grid points.
+        deriv : bool, optional
+            Whether to compute the derivative of squared difference w.r.t. model density.
+
+        Returns
+        -------
+        m : ndarray(N,)
+            The Tsallis divergence between density & model on the grid points.
+        dm : ndarray(N,)
+            The derivative of divergence w.r.t. model density evaluated on the grid points.
+            Only returned if `deriv=True`.
+
+        Notes
+        -----
+        - This does not impose non-negativity of the model density, unlike the Kullback-Leibler.
+        - Values of Model density that are less than `mask_value` are masked when used in
+            division and then replaced with the value of 0.
+        - As :math:`\alpha` parameter tends towards one, then this converges to the Kullback-Leibler.
+            This is particularly useful for trust-region methods that don't impose strict constraints
+            during the optimization procedure.
+
+        References
+        ----------
+        [1] Ayers, Paul W. "Information theory, the shape function, and the Hirshfeld atom."
+            Theoretical Chemistry Accounts 115.5 (2006): 370-378.
+        [2] Heidar-Zadeh, Farnaz, Ivan Vinogradov, and Paul W. Ayers. "Hirshfeld partitioning
+            from non-extensive entropies." Theoretical Chemistry Accounts 136.4 (2017): 54.
+
+        """
+        # check model density
+        if not isinstance(model, np.ndarray):
+            raise ValueError("Argument model should be {0} array.".format(density.shape))
+        if not isinstance(density, np.ndarray) or density.ndim != 1:
+            raise ValueError("Arguments density should be a 1D numpy array.")
+        if model.shape != density.shape:
+            raise ValueError(f"Model shape {model.shape} should be the same as density"
+                             f" {density.shape}.")
+        if not isinstance(deriv, bool):
+            raise TypeError(f"Deriv {type(deriv)} should be Boolean type.")
+
+        # compute ratio & replace masked values by 1.0
+        ratio = density / np.ma.masked_less_equal(model, self.mask_value)
+        ratio = np.ma.filled(ratio, fill_value=0.0)
+        value = density * (np.power(ratio, self.alpha - 1.0) - 1.0)
+        integrand = value / (self.alpha - 1.0)
+        if deriv:
+            return integrand, -np.power(ratio, self.alpha)
+        return integrand
