60102cb3ca737736c053c64e78caad84412519b4
Fanwang Meng <fanwang.meng@queensu.ca>
2024-11-01 22:41:16 -0400

    Add __init__.py to test module

---
+++ bfit/test/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# BFit is a Python library for fitting a convex sum of Gaussian
+# functions to any probability distribution
+#
+# Copyright (C) 2020- The QC-Devs Community
+#
+# This file is part of BFit.
+#
+# BFit is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# BFit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+# ---
+
+r"""Test module for bfit."""
